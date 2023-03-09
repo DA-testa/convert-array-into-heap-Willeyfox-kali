f67a4c8bee697fcc7b02cfecb7f1f55b724d9b2d
Oleksandr Voznenko <124575056+Willeyfox-kali@users.noreply.github.com>
2023-03-09 21:28:01 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -8,7 +8,9 @@ def build_heap(data):
     # try to achieve  O(n) and not O(n2)
     for i in range(n - 1, -1, -1):
         while 2 * i + 1 < n:
-            
+            j = 2 * i + 2
+            if j * 1 < n and data[i+1] < data[i]:
+                J = j + 1
 
     return swaps
 
@@ -18,11 +20,11 @@ def main():
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
-
+    n = input().strip()
     # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    if n == 'I':
+        n = int(input())
+        data = list(map(int, input().split()))
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
